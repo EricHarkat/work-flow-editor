2b149c9c687de868fa744def7bb1d068e270b70f
EricHarkat <eric.harkat@gmail.com>
2026-09-11 15:16:06 +0200

    feat : add css

--- front-work-flow/src/app/components/journal/journal.ts
+++ front-work-flow/src/app/components/journal/journal.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { JournalService } from '../../services/journal';
 
 @Component({
   selector: 'app-journal',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './journal.html',
   styleUrl: './journal.css',
 })
